88c6af2ddcfb8ceafef02d729147c937e2bf964d
yash <yash01verma@gmail.com>
2023-03-22 15:39:54 +0000

    latest code

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -14,7 +14,7 @@ import com.nbs.config.ConfigFactory;
 
 public class Test1 {
 
-	@Test(description = "To verify the title of the orangeHRM website")	 
+	@Test(description = "To test booking.com journey")	 
 	 public void launchApplication() throws InterruptedException{
 		 ChromeOptions chromeOptions = new ChromeOptions();
 		 chromeOptions.addArguments("--remote-allow-origins=*");
@@ -26,52 +26,39 @@ public class Test1 {
 		 JavascriptExecutor js = (JavascriptExecutor)driver;
  		 js.executeScript("window.scroll(0,200)");
  		 Thread.sleep(2000);
+ 		 
 		 driver.findElement(By.xpath("//button[@id='onetrust-accept-btn-handler']")).click();
 		 driver.findElement(By.xpath("//input[@name='ss']")).sendKeys("Devon");
 		 Thread.sleep(2000);
 		 driver.findElement(By.xpath("//input[@name='ss']")).sendKeys(Keys.ARROW_DOWN);
 		 driver.findElement(By.xpath("//input[@name='ss']")).sendKeys(Keys.ENTER);
 		 Thread.sleep(2000);
+		 	 
+
+		 String checkInDate = "2023-03-22";	
+		 String checkOutDate = "2023-03-22";	
 		 
+		 driver.findElement(By.xpath("//*[@data-date='" + checkInDate + "']")).click();
+		 driver.findElement(By.xpath("//*[@data-date='" + checkOutDate + "']")).click();		 
+		 driver.findElement(By.cssSelector("button[type='submit']")).click();
+		 driver.findElement(By.xpath("(//button[@type='button'])[14]")).click();
+		 driver.findElement(By.xpath("(//span[@class='e57ffa4eb5'][normalize-space()='See availability'])[1]")).click();
+		 driver.findElement(By.xpath("(//span[@class='bui-button__text'][normalize-space()='Reserve'])[1]")).click();
+		 driver.findElement(By.cssSelector(".bui-button__text.js-reservation-button__text")).click();
+
+		 driver.findElement(By.xpath("//input[@id='firstname']")).sendKeys("Exeter");
+		 driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys("Univ");
+		 driver.findElement(By.xpath("//input[@id='email']")).sendKeys("abc@gmail.com");
+		 driver.findElement(By.xpath("(//span[normalize-space()=\"I'm the main guest\"])[1]")).click();
+		 driver.findElement(By.xpath("//span[contains(@class,'bui-button__text js-button__text')]")).click();
 		 
-		 // click calendar
- 		 driver.findElement(By.xpath("//button[normalize-space()='Check-in Date']")).click();
-		 
- 		 Thread.sleep(3000);
- 
- 		 // select date
- 		 
- 		 
- 		 driver.findElement(By.cssSelector("[aria-label='28 March 2023']")).click();
- 		 Thread.sleep(2000);
- 	
- 		 
- 		 driver.findElement(By.xpath("span[aria-label='2 April 2023'] span")).click();
-		 			 
-//		 String checkInMonth = "March 2023";
-		 String checkInDate = "24";	
-//		 
-//		 while(true) {
-//			  String month = driver.findElement(By.xpath("//h3[normalize-space()='March 2023']")).getText();
-//			  if(month.equalsIgnoreCase(checkInMonth)) {
-//				  break;
-//			  }
-//			  else {
-//				  Thread.sleep(2000);
-//				  driver.findElement(By.xpath("//button[@type='button']//span[@class='b6dc9a9e69 e6c50852bd']//*[name()='svg']")).click();
-//			  }
-//	 }
-//		 
-//
-		 List<WebElement> dates = driver.findElements(By.xpath("//table[@class='aadb8ed6d3']//td"));
-		 for(WebElement element:dates) {
-			 String date = element.getText();
-			 if(date.equals(checkInDate)) {
-			 element.click();
-			 break;		 
-	}}}
-//	
- }
+		 driver.findElement(By.xpath("//input[@id='address1']")).sendKeys("victoria");
+		 driver.findElement(By.xpath("//input[@id='city']")).sendKeys("Swindon");
+		 driver.findElement(By.xpath("//input[@id='phone']")).sendKeys("7824678246");
+		 driver.findElement(By.xpath("button[name='book'] span[class='bui-button__text js-button__text']")).click();
+ 		
+ 		
+ }}
 
 				  
 	 
